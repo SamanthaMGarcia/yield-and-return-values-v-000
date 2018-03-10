a718abdf4ea0c363cf2c9354a1a77a98d3fafd44
Samantha Garcia <samantha.marie.garcia@gmail.com>
2018-03-10 22:50:30 +0000

    automatically backed up by learn

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -3,10 +3,11 @@ binding.pry
 def hello(array)
   i = 0
   collection = []
-  while i < array.length collection <<
-    yield(array[i])
+  while i < array.length
+  collection << yield(array[i])
     i += 1
   end
+  collection
 end
 
 hello(["Tim", "Tom", "Jim"]) { |name| "Hi, #{name}" }
